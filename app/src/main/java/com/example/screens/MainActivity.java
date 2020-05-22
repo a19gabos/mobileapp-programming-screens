205b0ca07ad63a15a9da2dff42fbd4d7a331df40
Gabriel <a19gabos@student.his.se>
2020-05-22 23:00:24 +0200

    Sart over

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -14,12 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.next);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                setContentView(R.layout.activity_second);
-            }
-        });
+        
     }
 }
